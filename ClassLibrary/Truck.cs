265c3aadb6a6d951cdfaf590175944408e36c3b3
Magomedali Gajiev <mag198421@gmail.com>
2024-11-10 14:28:40 +0300

    Добаил класс Truck

---
+++ ClassLibrary/Truck.cs
@@ -0,0 +1,21 @@
+﻿class Truck
+{
+    private readonly Warehouse _warehouse;
+    public int Capacity { get; }
+
+    public Truck(Warehouse warehouse, int capacity)
+    {
+        _warehouse = warehouse;
+        Capacity = capacity;
+    }
+
+    public void StartDelivery()
+    {
+        while (true)
+        {
+            // Логика загрузки продукции со склада
+            Thread.Sleep(new Random().Next(1000, 3000)); // Симуляция времени загрузки
+            Console.WriteLine($"Грузовик с вместимостью {Capacity} загружает продукцию.");
+        }
+    }
+}
