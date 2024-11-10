899683cda00dccc43b8bb189ee8e0df2bb9f5775
Magomedali Gajiev <mag198421@gmail.com>
2024-11-10 14:26:12 +0300

    Add project files.

---
+++ ClassLibrary/Factory.cs
@@ -0,0 +1,25 @@
+﻿class Factory
+{
+    public string Name { get; }
+    public string ProductName { get; }
+    public double ProductionRate { get; }
+    private Warehouse _warehouse;
+
+    public Factory(string name, string productName, double productionRate, Warehouse warehouse)
+    {
+        Name = name;
+        ProductName = productName;
+        ProductionRate = productionRate;
+        _warehouse = warehouse;
+    }
+
+    public void StartProduction()
+    {
+        while (true)
+        {
+            int units = (int)ProductionRate;
+            _warehouse.ReceiveProduct(Name, ProductName, units);
+            Thread.Sleep(1000); // Симуляция времени производства
+        }
+    }
+}
