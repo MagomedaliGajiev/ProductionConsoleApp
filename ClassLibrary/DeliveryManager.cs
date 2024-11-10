574eb5a166f3264b5ce9956397884d9fc2bb6525
Magomedali Gajiev <mag198421@gmail.com>
2024-11-10 14:30:18 +0300

    Добавил класс DeliveryManager

---
+++ ClassLibrary/DeliveryManager.cs
@@ -0,0 +1,15 @@
+﻿class DeliveryManager
+{
+    private readonly Warehouse _warehouse;
+
+    public DeliveryManager(Warehouse warehouse)
+    {
+        _warehouse = warehouse;
+    }
+
+    public void GenerateStatistics()
+    {
+        // Логика для расчета средних значений и итогов по доставкам грузовиков
+        Console.WriteLine("Генерация статистики...");
+    }
+}
